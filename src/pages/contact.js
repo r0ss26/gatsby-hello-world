03b34469fb8d7259d818dd8014dd6026e8757148
Ross Baker <rossbaker32@gmail.com>
2020-07-03 20:53:02 +1000

    Adds global stylesheet

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Header from '../components/header';
 
 const Contact = () => {
   return (
     <div style={{ color: `teal` }}>
       <Link to="/">Home</Link>
       <Header headerText="Contact" />
+      <p>Send us a message!</p>
     </div>
   )
 };
